21b310b6ff0bf97dbd6f1425673d2bce720d2b42
renanspvargas <renanspvargas@outlook.com>
2023-03-06 15:38:55 -0300

    retrieving and parsing mock json data

--- lib/app/modules/home/services/home_service_impl.dart
+++ lib/app/modules/home/services/home_service_impl.dart
@@ -1,10 +1,38 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+import 'package:lucky_investor/app/modules/home/constants/home_constants.dart';
 import 'package:lucky_investor/app/modules/home/models/stock_model.dart';
 import 'package:lucky_investor/app/modules/home/services/home_service.dart';
+import 'package:lucky_investor/app/modules/home/exceptions/home_exceptions.dart';
 
 class HomeServiceImpl implements HomeService {
   @override
-  Future<StockModel> getStockData(String stockCode) {
-    // TODO: implement getStockInfo
-    throw UnimplementedError();
+  Future<StockModel> getStockData(String stockCode) async {
+    final jsonList = await _loadAndDecode();
+
+    for (var jsonItem in jsonList) {
+      if (jsonItem['code'] == stockCode) {
+        try {
+          return StockModel.fromMap(jsonItem);
+        } catch (_) {
+          throw FailToConvertJsonException(
+              'Parece que o arquivo que recebemos esta com algum problema. Tente outro codigo ou aguarde alguns minutos.');
+        }
+      }
+    }
+    throw FailToLocateStockException(
+        'Nao foi possivel localizar nenhuma acao a partir dos dados digitados. Tente mudar a pesquisa!');
+  }
+
+  Future<List<Map<String, dynamic>>> _loadAndDecode() async {
+    try {
+      final String json = await rootBundle.loadString(HomeConstants.mockData);
+      final jsonList = List<Map<String, dynamic>>.from(jsonDecode(json));
+      return jsonList;
+    } catch (_) {
+      throw FailToLoadMockJsonException(
+          'Houve uma falha no carregamento dos dados');
+    }
   }
 }
