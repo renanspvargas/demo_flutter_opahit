925bf63ee97a913de0df6461df07350ffb2dcde6
renanspvargas <renanspvargas@outlook.com>
2023-03-06 15:14:32 -0300

    creating implementation for home service

---
+++ lib/app/modules/home/services/home_service_impl.dart
@@ -0,0 +1,10 @@
+import 'package:lucky_investor/app/modules/home/models/stock_model.dart';
+import 'package:lucky_investor/app/modules/home/services/home_service.dart';
+
+class HomeServiceImpl implements HomeService {
+  @override
+  Future<StockModel> getStockInfo(String stockCode) {
+    // TODO: implement getStockInfo
+    throw UnimplementedError();
+  }
+}
