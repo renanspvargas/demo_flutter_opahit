6221d94007e5628a1f06a85cdf785ba3c7c636b0
renanspvargas <renanspvargas@outlook.com>
2023-03-06 15:27:52 -0300

    adding mixins to home controller

--- lib/app/modules/home/home_controller.dart
+++ lib/app/modules/home/home_controller.dart
@@ -1,9 +1,20 @@
 import 'package:get/get.dart';
+import 'package:lucky_investor/app/core/mixins/loader_mixin.dart';
+import 'package:lucky_investor/app/core/mixins/messages_mixin.dart';
 import 'package:lucky_investor/app/modules/home/services/home_service.dart';
 
-class HomeController extends GetxController {
+class HomeController extends GetxController with LoaderMixin, MessagesMixin {
   final HomeService _homeService;
+  final _loading = false.obs;
+  final _message = Rxn<MessageModel>();
 
   HomeController({required HomeService homeService})
       : _homeService = homeService;
+
+  @override
+  void onInit() {
+    super.onInit();
+    loaderListener(_loading);
+    messageListener(_message);
+  }
 }
