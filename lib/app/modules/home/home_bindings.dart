e7b6db4b10fdd066a14620d4e3fe08066c015bac
renanspvargas <renanspvargas@outlook.com>
2023-03-06 15:26:01 -0300

    injecting home service

--- lib/app/modules/home/home_bindings.dart
+++ lib/app/modules/home/home_bindings.dart
@@ -1,6 +1,16 @@
 import 'package:get/get.dart';
+import 'package:lucky_investor/app/modules/home/home_controller.dart';
+import 'package:lucky_investor/app/modules/home/services/home_service.dart';
+import 'package:lucky_investor/app/modules/home/services/home_service_impl.dart';
 
 class HomeBindings implements Bindings {
   @override
-  void dependencies() {}
+  void dependencies() {
+    Get.lazyPut<HomeService>(
+      () => HomeServiceImpl(),
+    );
+    Get.lazyPut(
+      () => HomeController(homeService: Get.find()),
+    );
+  }
 }
--- lib/app/modules/home/home_controller.dart
+++ lib/app/modules/home/home_controller.dart
@@ -1,3 +1,9 @@
 import 'package:get/get.dart';
+import 'package:lucky_investor/app/modules/home/services/home_service.dart';
 
-class HomeController extends GetxController {}
+class HomeController extends GetxController {
+  final HomeService _homeService;
+
+  HomeController({required HomeService homeService})
+      : _homeService = homeService;
+}
