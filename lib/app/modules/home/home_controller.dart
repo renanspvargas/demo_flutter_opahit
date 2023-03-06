9a1d4909a1b9bc11e00ded827bdd56e0da30c110
renanspvargas <renanspvargas@outlook.com>
2023-03-06 15:41:07 -0300

    retrieving data from service and working on errors

--- lib/app/modules/home/home_controller.dart
+++ lib/app/modules/home/home_controller.dart
@@ -1,8 +1,11 @@
+import 'dart:developer';
+
 import 'package:get/get.dart';
 import 'package:lucky_investor/app/core/mixins/loader_mixin.dart';
 import 'package:lucky_investor/app/core/mixins/messages_mixin.dart';
 import 'package:lucky_investor/app/modules/home/models/stock_model.dart';
 import 'package:lucky_investor/app/modules/home/services/home_service.dart';
+import 'package:lucky_investor/app/modules/home/exceptions/home_exceptions.dart';
 
 class HomeController extends GetxController with LoaderMixin, MessagesMixin {
   final HomeService _homeService;
@@ -27,8 +30,34 @@ class HomeController extends GetxController with LoaderMixin, MessagesMixin {
   }
 
   Future<void> getStockData({stockCode = 'PETR4'}) async {
-    final result = await _homeService.getStockData(stockCode);
-    await Future.delayed(const Duration(seconds: 2));
-    stockData(result);
+    try {
+      _loading.toggle();
+      final result = await _homeService.getStockData(stockCode);
+      await Future.delayed(const Duration(seconds: 2));
+      stockData(result);
+      _loading.toggle();
+    } on FailToLoadMockJsonException catch (e, s) {
+      _loading.toggle();
+      log('Falha ao carregar JSON', error: e, stackTrace: s);
+      _message(MessageModel(
+        title: 'Erro',
+        message: e.message,
+      ));
+    } on FailToLocateStockException catch (e, s) {
+      _loading.toggle();
+      log('Pesquisa nao corresponde a nenhum stock listado',
+          error: e, stackTrace: s);
+      _message(MessageModel(
+        title: 'Erro',
+        message: e.message,
+      ));
+    } on FailToConvertJsonException catch (e, s) {
+      _loading.toggle();
+      log('Erro na conversao dos dados para model', error: e, stackTrace: s);
+      _message(MessageModel(
+        title: 'Erro',
+        message: e.message,
+      ));
+    }
   }
 }
