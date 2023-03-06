e2b2582718f674a5815556309023326a3e3259fe
renanspvargas <renanspvargas@outlook.com>
2023-03-06 15:30:51 -0300

    adding method to get stock data

--- lib/app/modules/home/home_controller.dart
+++ lib/app/modules/home/home_controller.dart
@@ -1,20 +1,34 @@
 import 'package:get/get.dart';
 import 'package:lucky_investor/app/core/mixins/loader_mixin.dart';
 import 'package:lucky_investor/app/core/mixins/messages_mixin.dart';
+import 'package:lucky_investor/app/modules/home/models/stock_model.dart';
 import 'package:lucky_investor/app/modules/home/services/home_service.dart';
 
 class HomeController extends GetxController with LoaderMixin, MessagesMixin {
   final HomeService _homeService;
   final _loading = false.obs;
   final _message = Rxn<MessageModel>();
+  final Rxn<StockModel> stockData = Rxn();
 
   HomeController({required HomeService homeService})
       : _homeService = homeService;
+
+  @override
+  void onReady() async {
+    super.onReady();
+    await getStockInfo();
+  }
 
   @override
   void onInit() {
     super.onInit();
     loaderListener(_loading);
     messageListener(_message);
+  }
+
+  Future<void> getStockInfo({stockCode = 'PETR4'}) async {
+    final result = await _homeService.getStockInfo(stockCode);
+    await Future.delayed(const Duration(seconds: 2));
+    stockData(result);
   }
 }
